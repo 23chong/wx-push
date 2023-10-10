216d606c578eb84b8c4595e33d7f7de3a6f6396d
23chong <147476874+23chong@users.noreply.github.com>
2023-10-10 14:08:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,1 @@
-module.exports = {
-  appid: "wx2f6de27a8fdd8afd",
-  appsecret: "dfcf8e172e5fc357cce5e3b57e71f095",
-  template_id: "yCZN7M-mWMGkrY1S065RoIiIMRt4jiAu6ozGCmLHZi0",
-  touser: "oE6KN6BQOCLkPwC7jaeVDU3sR8qk",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "澳門",
-  first_date: "2023-05-27",
-};
